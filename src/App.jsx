18b6fdb1281b64f26ba0741d4879586978849ae7
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2023-12-25 21:19:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Hello() {
+    return (
+        <React.StrictMode>
+            <h1>Hello, World!</h1>
+            <p>This is a test.</p>
+        </React.StrictMode>
+    )
+}
